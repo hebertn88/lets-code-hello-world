7f5788e3138eb2fe76c3c36990db75e42d7694ce
Hebert do Nascimento <hebertn88@gmail.com>
2022-09-11 10:42:05 -0300

    Aula 03 - Variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //AULA 02 - SETUP
+        System.out.println("Hello, World!");
+
+        //AULA 03 - VARIAVEIS
+
+        //Variaveis String
+        
+        String nome = "Lets Code";
+        nome = "Hebert";
+        
+        String sobrenome;
+        sobrenome = "do Nascimento";
+        System.out.println("Olá, " + nome + " " + sobrenome + "!");
+
+        //Variáveis numericas
+        int a;
+        int b = 2;
+        a = 3;
+
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b; //(float) transforma resultado int em float "Cast"
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
     }
 }
